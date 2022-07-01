9169494502e323397fa7108ea81f79c43af63945
张振华 <hellozzhgg@163.com>
2022-07-01 19:37:25 +0800

    无限滑动列表练习

---
+++ DemoProject/Assets/UI/Scripts/InitScroll.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitScroll : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject ob = GameObject.Find("Scroll View");
+        InfiniteScroll infiniteScroll = ob.GetComponent<InfiniteScroll>();
+        infiniteScroll.SetTotalCount(1000);
+        infiniteScroll.Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
