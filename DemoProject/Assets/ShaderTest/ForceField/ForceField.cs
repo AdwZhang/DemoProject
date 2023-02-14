2f33e7a6e279d09a4be0648ef71ef6a12e559ad3
张振华 <hellozzhgg@163.com>
2023-02-14 19:17:52 +0800

    能量护盾 脚本修改

--- DemoProject/Assets/ShaderTest/ForceField/ForceField.cs
+++ DemoProject/Assets/ShaderTest/ForceField/ForceField.cs
@@ -1,20 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
+using AmplifyShaderEditor;
 using UnityEngine;
 
 [ExecuteInEditMode]
 public class ForceField : MonoBehaviour
 {
+    public ParticleSystem ps;
+    public string triggerTag = "ForceField";
+    public float clicksPerSecond = 0.1f;
+    private float clickTimer = 0.0f;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    void DoRayCast()
+    {
+        RaycastHit hitInfo;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hitInfo, 1000) && hitInfo.transform.CompareTag(triggerTag))
+        {
+            ps.transform.position = hitInfo.point;
+            ps.Emit(1);
+        }
+    }
+    
     // Update is called once per frame
     void Update()
     {
-        Shader.SetGlobalVector("HitPosition",transform.position);
-        Shader.SetGlobalFloat("HitSize",transform.localScale.x);
+        clickTimer += Time.deltaTime;
+        if (Input.GetMouseButton(0))
+        {
+            if (clickTimer > clicksPerSecond)
+            {
+                clickTimer = 0.0f;
+                DoRayCast();
+            }   
+        }
+        
+        /*Shader.SetGlobalVector("HitPosition",transform.position);
+        Shader.SetGlobalFloat("HitSize",transform.localScale.x);*/
     }
 }
